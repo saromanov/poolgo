85d08f657d0fc4fa42cb3b96bdf4d76da33f2af9
Sergey Romanov <xxsmotur@gmail.com>
2015-03-25 22:52:43 +0500

    Some updates

--- poolgo.go
+++ poolgo.go
@@ -3,6 +3,7 @@ package poolgo
 import
 (
 	"sync"
+	"fmt"
 )
 
 type
@@ -12,9 +13,12 @@ type
 
 type Pool struct{
 	Mutex sync.RWMutex
-	//map of workers. Need when  to call worker by name
+	//map of workers. Need when to call worker by name
 	Workers map[string] workfunc
+	//just slice of workers
+	UnnamedWorkers[] workfunc
 	Poolnums int
+	Data [] interface{}
 }
 
 //Create basic pool
@@ -22,6 +26,7 @@ func Create (poolnum int) (*Pool){
 	pool := new(Pool)
 	pool.Poolnums = poolnum
 	pool.Workers = make(map[string]workfunc)
+	pool.UnnamedWorkers = make([]workfunc, poolnum)
 	return pool
 }
 
@@ -29,14 +34,27 @@ func Create (poolnum int) (*Pool){
 func (pool*Pool) AppendFunc(name string, value func(interface{}) interface{}){
 	pool.Workers[name] = value
 }
-//Append new job
-func (pool*Pool) Append(value interface{}){
-	pool.Mutex.Lock()
-	defer pool.Mutex.Unlock()
+
+//Append new work to unnamed slice
+//value is function
+func (pool*Pool) AppendFuncs(value func(interface{}) interface{}){
+	for i := 0; i < pool.Poolnums; i++ {
+		pool.UnnamedWorkers = append(pool.UnnamedWorkers, value)
+	}
+}
+
+//Add data to workers
+func (pool*Pool) AddData(data []interface{}){
+	pool.Data = data
 }
 
 func (pool*Pool) Run(name string, param interface{}){
-
+	for i := 0; i < pool.Poolnums; i++ {
+		go func (param interface{}) {
+			result := pool.UnnamedWorkers[i](param)
+			fmt.Println(result)
+		}(pool.Data[i])
+	}
 }
 
 //Run jobs in first case - set list of params
@@ -44,8 +62,14 @@ func(pool*Pool) RunWithValues(name string, params[] interface{}){
 	for i := 0; i < pool.Poolnums; i++ {
 		go func(param int){
 			result := pool.Workers[name](params[param])
+			fmt.Println(result)
 		}(i)
 	}
+}
+
+//Remove all workers in the case if worker are stopped
+func (pool*Pool) RemoveAll() {
+
 }
 
 func (pool*Pool) Close(){
